039f4101e983f4d89c73fa7e9d51a90cd3fcc47e
dongwen <dongwen@migu.cn>
2018-11-05 12:14:34 +0800

    modify dir

---
+++ gms-core/src/main/java/com/taoyuan/gms/core/sitemanage/account/service/AccountService.java
@@ -0,0 +1,11 @@
+package com.taoyuan.gms.core.sitemanage.account.service;
+
+
+import java.util.List;
+
+import com.taoyuan.gms.api.sitemanage.account.AccountDto;
+
+public interface AccountService {
+
+	public List<AccountDto> getAccounts();
+}
