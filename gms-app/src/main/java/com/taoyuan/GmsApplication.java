86c9751ae87af3385f0e4411ce8f353b44269d5c
zopnh <huzhengchao01@saicmotor.com>
2018-11-12 16:18:49 +0800

    增加亏损返利计算定时任务，每日零时计算

--- gms-app/src/main/java/com/taoyuan/GmsApplication.java
+++ gms-app/src/main/java/com/taoyuan/GmsApplication.java
@@ -2,10 +2,12 @@ package com.taoyuan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 //@EnableWebMvc
+@EnableScheduling
 public class GmsApplication {
 
 	public static void main(String[] args) {
---
+++ gms-core/src/main/java/com/taoyuan/gms/job/QuartzConfig.java
@@ -0,0 +1,26 @@
+package com.taoyuan.gms.job;
+
+import com.taoyuan.gms.job.adminmanage.LossRabateJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 定时任务配置
+ */
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetail dailyLossRabateJobTetail() {
+        return JobBuilder.newJob(LossRabateJob.class).withIdentity("dailyLossRabateJobTetail").storeDurably().build();
+    }
+
+    @Bean
+    public Trigger dailyLossRabateJobTrigger() {
+        return TriggerBuilder.newTrigger().forJob(dailyLossRabateJobTetail())
+                .withIdentity("dailyLossRabateJobTetail")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+                .build();
+    }
+}
