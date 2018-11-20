51b8d475e0ee802fa0b9561565af81bc3dc3dc3b
zopnh <huzhengchao01@saicmotor.com>
2018-11-20 14:02:47 +0800

    增加等级配置功能

--- gms-app/src/test/java/com/taoyuan/gms/main/AppTest.java
+++ gms-app/src/test/java/com/taoyuan/gms/main/AppTest.java
@@ -3,7 +3,9 @@ package com.taoyuan.gms.main;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.taoyuan.GmsApplication;
 import com.taoyuan.gms.core.adminmanage.service.IGameSettingService;
+import com.taoyuan.gms.core.adminmanage.service.ILevelSettingService;
 import com.taoyuan.gms.model.entity.admin.web.GameSettingEntity;
+import com.taoyuan.gms.model.entity.admin.web.LevelSettingEntity;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -28,8 +30,12 @@ import java.util.Map;
 @WebAppConfiguration
 @Slf4j
 public class AppTest extends TestCase {
+
     @Autowired
     private IGameSettingService gameSettingService;
+
+    @Autowired
+    private ILevelSettingService levelSettingService;
 
     public AppTest(){}
 
@@ -59,13 +65,13 @@ public class AppTest extends TestCase {
 
     @org.junit.Test
     public void testUpdateGameSetting() {
-        List<GameSettingEntity> games = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
-            GameSettingEntity entity = new GameSettingEntity();
-            entity.setGameName("游戏" + i);
-            games.add(entity);
-        }
-        gameSettingService.saveOrUpdateBatch(games);
+//        List<GameSettingEntity> games = new ArrayList<>();
+//        for (int i = 1; i < 10; i++) {
+//            GameSettingEntity entity = new GameSettingEntity();
+//            entity.setGameName("游戏" + i);
+//            games.add(entity);
+//        }
+//        gameSettingService.saveOrUpdateBatch(games);
     }
 
     @org.junit.Test
@@ -73,5 +79,24 @@ public class AppTest extends TestCase {
 //        Map<String, Object> map = gameSettingService.getMap(new QueryWrapper<GameSettingEntity>());
 //        Assert.assertNotNull(map);
 //        log.info(String.valueOf(map));
+    }
+
+    @org.junit.Test
+    public void testLevelSetting(){
+        List<LevelSettingEntity> levelSettingEntities = new ArrayList<LevelSettingEntity>();
+        for(int i =0;i<8;i++){
+            String name = "vip" +i;
+            LevelSettingEntity entity = new LevelSettingEntity();
+            entity.setId(i);
+            entity.setName(name);
+            entity.setExperience(1000*(i+1));
+            entity.setDrawCondition(100);
+            entity.setDrawTimes(10);
+            entity.setRechargeCommission(2);
+            entity.setRelieveGoldenCoin(50);
+            entity.setCashPrizeDiscount(100+i);
+            levelSettingEntities.add(entity);
+        }
+        levelSettingService.saveOrUpdateBatch(levelSettingEntities);
     }
 }
