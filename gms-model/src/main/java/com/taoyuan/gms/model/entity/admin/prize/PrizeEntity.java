82d0c96715db8ea69daee8d66dd031c5c2455602
supermanhzc <supermanhzc@163.com>
2018-11-27 21:59:29 +0800

    奖品管理功能优化

--- gms-model/src/main/java/com/taoyuan/gms/model/entity/admin/prize/PrizeEntity.java
+++ gms-model/src/main/java/com/taoyuan/gms/model/entity/admin/prize/PrizeEntity.java
@@ -40,43 +40,43 @@ public class PrizeEntity implements Serializable {
     private String details;
 
     public void update(PrizeEntity prizeEntity) {
-        if(null==prizeEntity){
+        if (null == prizeEntity) {
             return;
         }
 
-        if(null!=prizeEntity.getName()) {
+        if (null != prizeEntity.getName()) {
             setName(prizeEntity.getName());
         }
 
-        if(null!=prizeEntity.getClassify()) {
+        if (null != prizeEntity.getClassify()) {
             setClassify(prizeEntity.getClassify());
         }
 
-        if(!prizeEntity.getBasicPrice().equals(BigDecimal.ZERO)) {
+        if (null != prizeEntity.getBasicPrice() && !prizeEntity.getBasicPrice().equals(BigDecimal.ZERO)) {
             setBasicPrice(prizeEntity.getBasicPrice());
         }
 
-        if(null!=prizeEntity.getIcon()) {
+        if (null != prizeEntity.getIcon()) {
             setIcon(prizeEntity.getIcon());
         }
 
-        if(null!=prizeEntity.getDetails()) {
+        if (null != prizeEntity.getDetails()) {
             setDetails(prizeEntity.getDetails());
         }
 
-        if(prizeEntity.getCardType()!=0){
+        if (prizeEntity.getCardType() != 0) {
             setCardType(prizeEntity.getCardType());
         }
 
-        if(prizeEntity.getAutoDispatch()!=0){
+        if (prizeEntity.getAutoDispatch() != 0) {
             setAutoDispatch(prizeEntity.getAutoDispatch());
         }
 
-        if(prizeEntity.getConverted()!=0){
+        if (prizeEntity.getConverted() != 0) {
             setConverted(prizeEntity.getConverted());
         }
 
-        if(prizeEntity.getStock()!=0){
+        if (prizeEntity.getStock() != 0) {
             setStock(prizeEntity.getStock());
         }
     }
