d097d20098693b0786f33cc783c2d47b17703d65
dongwen <dongwen@migu.cn>
2018-12-02 00:56:07 +0800

    modify balance code

--- gms-api/src/main/java/com/taoyuan/gms/api/admin/UserBalanceChangeApi.java
+++ gms-api/src/main/java/com/taoyuan/gms/api/admin/UserBalanceChangeApi.java
@@ -3,6 +3,8 @@ package com.taoyuan.gms.api.admin;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.taoyuan.framework.common.http.TyResponse;
 import com.taoyuan.gms.model.dto.admin.UserBalanceChangeDto;
+import com.taoyuan.gms.model.dto.admin.account.QueryAccountBalanceRequest;
+import com.taoyuan.gms.model.dto.admin.account.UpdateAccountBalanceRequest;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,8 @@ import java.util.Map;
 public interface UserBalanceChangeApi {
 
     @RequestMapping(value = "/get", method = RequestMethod.POST)
-    IPage<Map<String, Object>> getAllBalanceChangeHistory(@RequestParam("pageIndex") Integer pageIndex, @RequestParam("pageSize") Integer pageSize);
+    IPage<Map<String, Object>> getAllBalanceChangeHistory(@RequestBody QueryAccountBalanceRequest queryAccountBalanceRequest);
 
     @RequestMapping(value = "/change", method = RequestMethod.POST)
-    TyResponse changeBalance(@RequestBody UserBalanceChangeDto userBalanceChangeDto);
+    TyResponse changeBalance(@RequestBody UpdateAccountBalanceRequest updateAccountBalanceRequest);
 }
--- gms-core/src/main/java/com/taoyuan/gms/core/adminmanage/controller/UserBalanceChangeController.java
+++ gms-core/src/main/java/com/taoyuan/gms/core/adminmanage/controller/UserBalanceChangeController.java
@@ -1,14 +1,19 @@
 package com.taoyuan.gms.core.adminmanage.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.taoyuan.framework.bs.controller.TyBaseController;
 import com.taoyuan.framework.common.entity.TyUserRolePermission;
 import com.taoyuan.framework.common.exception.TyBusinessException;
+import com.taoyuan.framework.common.exception.TyExceptionUtil;
 import com.taoyuan.framework.common.http.TyResponse;
 import com.taoyuan.framework.common.http.TySession;
 import com.taoyuan.framework.common.http.TySuccessResponse;
 import com.taoyuan.gms.api.admin.UserBalanceChangeApi;
 import com.taoyuan.gms.core.adminmanage.service.IUserBalanceChangeService;
 import com.taoyuan.gms.model.dto.admin.UserBalanceChangeDto;
+import com.taoyuan.gms.model.dto.admin.account.QueryAccountBalanceRequest;
+import com.taoyuan.gms.model.dto.admin.account.UpdateAccountBalanceRequest;
 import com.taoyuan.gms.model.entity.admin.account.UserBalanceChangeEntity;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,17 +23,25 @@ import java.util.Date;
 import java.util.Map;
 
 @RestController
-public class UserBalanceChangeController implements UserBalanceChangeApi{
+public class UserBalanceChangeController extends TyBaseController implements UserBalanceChangeApi{
     @Autowired
     private IUserBalanceChangeService uerBalanceChangeService;
 
     @Override
-    public IPage<Map<String, Object>> getAllBalanceChangeHistory(Integer pageIndex, Integer pageSize) {
+    public IPage<Map<String, Object>> getAllBalanceChangeHistory(QueryAccountBalanceRequest queryAccountBalanceRequest) {
+        QueryWrapper wrapper = new QueryWrapper();
+        if(null != queryAccountBalanceRequest.getId()){
+//            wrapper.lambda().eq(UserBalanceChangeEntity::getId, queryAccountBalanceRequest.getId());
+        }
+        uerBalanceChangeService.page(this.getPage(queryAccountBalanceRequest), null);
         return null;
     }
 
     @Override
-    public TyResponse changeBalance(UserBalanceChangeDto userBalanceChangeDto) {
-        return uerBalanceChangeService.changeBalance(userBalanceChangeDto);
+    public TyResponse changeBalance(UpdateAccountBalanceRequest updateAccountBalanceRequest) {
+        if(uerBalanceChangeService.changeBalance(updateAccountBalanceRequest)){
+            return new TySuccessResponse("user balance changed successfully.");
+        }
+        throw new TyBusinessException("user balance changed failed.");
     }
 }
