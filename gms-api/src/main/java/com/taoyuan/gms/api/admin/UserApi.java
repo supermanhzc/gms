7604f4a63d330e7e0f04f703e75e584c3867c4d5
dongwen <dongwen@migu.cn>
2018-12-03 18:52:47 +0800

    modify user logic

--- gms-api/src/main/java/com/taoyuan/gms/api/admin/UserApi.java
+++ gms-api/src/main/java/com/taoyuan/gms/api/admin/UserApi.java
@@ -1,13 +1,11 @@
 package com.taoyuan.gms.api.admin;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.taoyuan.framework.common.entity.TyPageEntity;
 import com.taoyuan.framework.common.http.TyResponse;
 import com.taoyuan.gms.model.dto.admin.account.*;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @Api(value = "会员管理")
 @RequestMapping("/users")
@@ -16,7 +14,7 @@ public interface UserApi{
     TyResponse queryUsers(@RequestBody QueryAccountRequest queryAccountRequest);
 
     @RequestMapping(value = "/getProxy", method = RequestMethod.POST)
-    TyResponse queryProxys(TyPageEntity queryProxyRequest);
+    TyResponse queryProxys(@RequestBody TyPageEntity queryProxyRequest);
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
     TyResponse deleteUser(@PathVariable("id") Long id);
@@ -34,7 +32,7 @@ public interface UserApi{
     TyResponse updateProxy(@RequestBody UpdateProxyRequest updateProxyRequest);
 
     @RequestMapping(value = "/createAdmin", method = RequestMethod.POST)
-    TyResponse createAdmin(CreateAdminRequest createAdminRequest);
+    TyResponse createAdmin(@RequestBody CreateAdminRequest createAdminRequest);
 
     @RequestMapping(value = "/updateAdmin", method = RequestMethod.POST)
     TyResponse updateAdmin(@RequestBody UpdateAdminRequest updateAdminRequest);
--- gms-model/src/main/java/com/taoyuan/gms/model/dto/admin/account/UpdateAdminRequest.java
+++ gms-model/src/main/java/com/taoyuan/gms/model/dto/admin/account/UpdateAdminRequest.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class UpdateAdminRequest extends AccountBaseRequest {
-
+    private Long id;
 }
