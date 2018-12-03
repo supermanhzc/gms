dfb24b6af4377c93a95b56eb2024fb73405e6b60
supermanhzc <supermanhzc@163.com>
2018-12-03 23:12:01 +0800

    代码重构

--- gms-core/src/main/java/com/taoyuan/gms/core/proxymanage/service/impl/FirstchargeRebateServiceImpl.java
+++ gms-core/src/main/java/com/taoyuan/gms/core/proxymanage/service/impl/FirstchargeRebateServiceImpl.java
@@ -5,7 +5,9 @@ import com.taoyuan.framework.bs.service.TyBaseServiceImpl;
 import com.taoyuan.gms.core.proxymanage.dao.FirstchargeRebateMapper;
 import com.taoyuan.gms.core.proxymanage.service.IFirstchargeRebateService;
 import com.taoyuan.gms.model.entity.proxy.FirstchargeRebateEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FirstchargeRebateServiceImpl extends TyBaseServiceImpl<FirstchargeRebateMapper, FirstchargeRebateEntity> implements IFirstchargeRebateService {
     @Override
     public boolean exist(Long id) {
