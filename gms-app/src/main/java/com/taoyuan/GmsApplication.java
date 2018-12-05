f94a5cd6a0b4c8dc35de0b3a822bfe0853c8a5eb
dongwen <dw96998@163.com>
2018-12-06 00:25:36 +0800

    add eureka service

--- gms-app/src/main/java/com/taoyuan/GmsApplication.java
+++ gms-app/src/main/java/com/taoyuan/GmsApplication.java
@@ -4,11 +4,13 @@ import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableScheduling
 public class GmsApplication {
@@ -25,8 +27,8 @@ public class GmsApplication {
     }
 
     @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
         tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
         return tomcat;
     }
