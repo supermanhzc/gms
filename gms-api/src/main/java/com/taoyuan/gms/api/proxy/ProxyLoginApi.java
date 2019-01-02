300105bea49e64383a8bd2981e13fb4e0137e296
supermanhzc <supermanhzc@163.com>
2019-01-02 21:48:34 +0800

    管理员，代理，会员登录分开管理

--- gms-api/src/main/java/com/taoyuan/gms/api/proxy/ProxyLoginApi.java
+++ gms-api/src/main/java/com/taoyuan/gms/api/proxy/ProxyLoginApi.java
@@ -3,15 +3,16 @@ package com.taoyuan.gms.api.proxy;
 import com.taoyuan.framework.common.entity.TyUser;
 import com.taoyuan.framework.common.http.TyResponse;
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 代理操作，登录等
  */
-@Api(value = "代理用户操作")
+@Api(value = "代理登录操作")
 @RequestMapping("/login/proxy")
-public interface ProxyUserApi {
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    TyResponse login(TyUser userInfo);
+public interface ProxyLoginApi {
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    TyResponse login(@RequestBody TyUser userInfo);
 }
--- gms-core/src/main/java/com/taoyuan/gms/core/adminmanage/controller/AdminLoginController.java
+++ gms-core/src/main/java/com/taoyuan/gms/core/adminmanage/controller/AdminLoginController.java
@@ -1,10 +1,11 @@
-package com.taoyuan.gms.core.proxymanage.controller;
+package com.taoyuan.gms.core.adminmanage.controller;
 
 import com.taoyuan.framework.bs.aspect.OperControllerLog;
 import com.taoyuan.framework.common.entity.TyUser;
 import com.taoyuan.framework.common.http.TyResponse;
-import com.taoyuan.gms.api.proxy.ProxyLoginApi;
+import com.taoyuan.gms.api.admin.AdminLoginApi;
 import com.taoyuan.gms.core.adminmanage.dao.UserMapper;
+import com.taoyuan.gms.core.proxymanage.controller.AbstractLoginController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,25 +15,24 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-public class ProxyUserController extends AbstractLoginController implements ProxyLoginApi {
+public class AdminLoginController extends AbstractLoginController implements AdminLoginApi {
 
     @Autowired
     private UserMapper userMapper;
 
     @Override
-    @OperControllerLog(module = "代理登录管理", type = "代理登录")
+    @OperControllerLog(module = "管理员登录管理", type = "管理员登录")
     public TyResponse login(@RequestBody TyUser userInfo) {
         return super.login(userInfo);
     }
 
     @Override
     public Map getUserInfo(String username) {
-        return userMapper.getProxyByUserName(username);
+        return userMapper.getAdminByUserName(username);
     }
 
     @Override
     public int getUserType() {
-        return 2;
+        return 3;
     }
-
 }
